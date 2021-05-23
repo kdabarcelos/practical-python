e0d3f968b995981ad61c2578051f7232d6658e50
Karina Barcelos <kdabarcelos@mun.ca>
2021-05-23 19:24:17 -0230

    Exer. 1.9

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -6,17 +6,18 @@ payment = 2684.11
 total_paid = 0.0
 extra = 1000
 month = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
 
 while principal > 0:
     
     month = month + 1
 
-    if month <= 12:
+    if month > extra_payment_start_month and month < extra_payment_end_month:
         principal = principal * (1+rate/12) - payment - extra
         total_paid = total_paid + payment + extra
     else:
         principal = principal * (1+rate/12) - payment
         total_paid = total_paid + payment
 
-    
 print('Total paid', total_paid, 'over', month, 'months.')
