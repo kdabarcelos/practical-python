99d1812d6b7dde459a9a69a0d6b352e2d47f255b
Karina Barcelos <kdabarcelos@mun.ca>
2021-05-26 20:16:59 -0230

    Exer. 2.7

--- Work/report.py
+++ Work/report.py
@@ -1,9 +1,9 @@
 # report.py
-#
-# Exercise 2.4
+# Exercise 2.4 - simpler
+
+# Exercise 2.5 - below
 
 import csv
-from types import DynamicClassAttribute
 
 def read_portfolio(filename):
     '''Computes the total cost (shares*price) of a portfolio file'''
@@ -24,8 +24,52 @@ def read_portfolio(filename):
 
 portfolio =  read_portfolio('Data/portfolio.csv')
 
-# to see the portfolio: 
+# to see the portfolio: 2.4
 #print (portfolio)
 
+#better visualization 2.5
 from pprint import pprint
 pprint(portfolio)
+
+# Exercise 2.6
+
+#import csv
+#f = open('Data/prices.csv', 'r')
+#rows = csv.reader(f)
+#for row in rows:
+#    print(row)
+
+def read_prices(filename):
+    prices = {}
+    with open(filename) as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+    return prices
+
+prices =  read_prices('Data/prices.csv')
+
+from pprint import pprint
+pprint(prices)
+
+#compute basic total cost so far
+
+total_before = 0 # start with 0
+
+for sum in portfolio:
+    total_before += sum['price']*sum['shares'] # multiply dic with ['key names']
+
+# current values to compare with the prior ones
+
+total_amount_now = 0
+
+for sum in portfolio:
+    total_amount_now += prices[sum['name']]*sum['shares']
+
+#compute loss/gain
+print ('Value in market before was', total_before)
+print ('Value in market now is', total_amount_now)
+print ('Gain/loss at the end', total_amount_now - total_before)
