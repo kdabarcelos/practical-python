c4ea5184ca468a8cee4924727adbdd157fc58d35
Karina Barcelos <kdabarcelos@mun.ca>
2021-05-26 19:32:55 -0230

    Exer 2.6

--- Work/report.py
+++ Work/report.py
@@ -3,26 +3,29 @@
 # Exercise 2.4
 
 import csv
+from types import DynamicClassAttribute
 
 def read_portfolio(filename):
     '''Computes the total cost (shares*price) of a portfolio file'''
     portfolio = []
-    total = 0.0
-
+    
     with open(filename) as f:
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
-            portfolio.append(holding)
-        
-        for name, shares, price in portfolio:
-            total += shares*price
-
-        print("Total is", total)
-    
+            naming_value = {
+                'name': row[0],
+                'shares': int(row[1]),
+                'price': float(row[2])
+            }
+            portfolio.append(naming_value)
+   
     return portfolio
 
 portfolio =  read_portfolio('Data/portfolio.csv')
 
-# to see the portfolio: print (portfolio)
+# to see the portfolio: 
+#print (portfolio)
+
+from pprint import pprint
+pprint(portfolio)
