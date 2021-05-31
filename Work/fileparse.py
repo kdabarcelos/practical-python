a1f2c9fa59e642278ebcedddcc135e165ddb2016
Karina Barcelos <kdabarcelos@mun.ca>
2021-05-30 21:46:34 -0230

    Exer 3.7

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,34 +1,42 @@
 # fileparse.py
 import csv
 
-def parse_csv(filename, select=None):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter = ','):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
-        rows = csv.reader(f)
+        rows = csv.reader(f, delimiter=delimiter)
 
-        # Read the file headers
-        headers = next(rows)
+        # Read the file headers in case there is one
+        headers = next(rows) if has_headers else []
 
-        # If a column selector was given, find indices of the specified columns.
-        # Also narrow the set of headers used for resulting dictionaries
+        # If a column was chosen
         if select:
             indices = [headers.index(colname) for colname in select]
-            headers = select
-        else:
-            indices = []
-
-        records = []
+            headers = select   
+        
+        records = []   
         for row in rows:
-            if not row:    # Skip rows with no data
+            # Skip rows with blanket data
+            if not row:    
                 continue
-            # Filter the row if specific columns were selected
-            if indices:
-                row = [ row[index] for index in indices ]
+            
+            # If column indices are selected, pick them out
+            if select:
+                row = [ row[index] for index in indices]
 
-            # Make a dictionary
-            record = dict(zip(headers, row))
+            # Converting to the correct type
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
+
+            # Creating dictionary
+            if headers:
+                record = dict(zip(headers, row))
+            
+            # Creating tuple instead
+            else:
+                record = tuple(row)
             records.append(record)
 
     return records
