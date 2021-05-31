60c102a44934f529c308e1e2172b474888a2b1f5
Karina Barcelos <kdabarcelos@mun.ca>
2021-05-31 11:12:01 -0230

    Changes that were not committed

--- Work/report.py
+++ Work/report.py
@@ -1,38 +1,16 @@
 # report.py
 
 # read datafile
-import csv
+import fileparse
 
 def read_portfolio(filename):
     '''Read a stock portfolio file into a list of dictionaries with keys
     name, shares, and price.'''
-    portfolio = []
-    
-    with open(filename) as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        for row in rows:
-            record = dict(zip(headers, row))
-            naming_value = {
-                'name': row[0],
-                'shares': int(row[1]),
-                'price': float(row[2])
-            }
-            portfolio.append(naming_value)
-   
-    return portfolio
+    return fileparse.parse_csv(filename, select=['name', 'shares', 'price'], types = [str, int, float])
 
 def read_prices(filename):
     '''Read a stock prices file into a dictionarie with keys name and price data'''
-    prices = {}
-    with open(filename) as f:
-        rows = csv.reader(f)
-        for row in rows:
-            try:
-                prices[row[0]] = float(row[1])
-            except IndexError:
-                pass
-    return prices
+    return dict(fileparse.parse_csv(filename, types=[str,float], has_headers=False))
 
 def make_report(portfolio, prices):
     '''Make a list (name, shares, price, and change) of tuples'''
